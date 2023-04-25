f4a3d2f0b1df57852ba8f6ed9824140569f8129a
ericmuthoka <muthokaerick@gmail.com>
2023-04-25 08:44:38 -0700

    printf

--- 0_printf.c
+++ 0_printf.c
@@ -12,6 +12,10 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	while (*format != '\0')
 	{
+		if (format == NULL)
+		{
+			return (-1);
+		}
 		if (*format == '%')
 		{
 			format++;
